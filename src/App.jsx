f34349ab0ae398b41501c14a7d154a83ae278305
akanshaaa19 <asakhre2002@gmail.com>
2022-12-24 13:15:16 +0530

    worked on orbit page and added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import { React, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 import Build from "./Components/Build";
 import NavBar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Orbit from "./Components/Orbit/Orbit";
+import About from "./Components/About";
+import { Route } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Hero />
-      <Build />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+
+        <Route path="/" exact>
+          <Hero />
+        </Route>
+
+        <Route path="/home">
+          <Hero />
+        </Route>
+
+        <Route path="/build">
+          <Build />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
